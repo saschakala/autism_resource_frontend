0a66d63c10c440d508bccb63458fc81ecddaa1f3
“saschakala” <mxsaschakala@gmail.com>
2021-01-20 12:09:19 -0500

    added bootstrap cdn to index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import sourceReducer from './reducers/sourceReducer'
+import sourceReducer from './reducers/sourceReducer';
+
 
 // import reportWebVitals from './reportWebVitals';
 
