85f363dcb002cdab122ed7e56d6f9b6e0f737ad0
Sascha Kala <mxsaschakala@gmail.com>
2021-01-21 15:16:09 -0500

    source tthumbnails renders on page

---
+++ src/components/SourceThumbnails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+const SourceThumbnails = () => {
+    return (
+        <Container>
+            <div>test</div>
+        </Container>
+
+    )
+}
+
+export default SourceThumbnails
