da6681ea5579a0218a450f5664f95c7ffbfbc7aa
Sascha Kala <mxsaschakala@gmail.com>
2021-01-21 15:04:12 -0500

    added congole.log fetch to App to test and created JumboTron component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import JumboTron from './components/JumboTron'
 
 
 class App extends React.Component {
+
+componentDidMount() {
+  fetch('http://localhost:3000/api/v1/sources/books')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+
   render() {
     return (
       <div className="App">
-      Word 
+        <JumboTron/>
       </div>
     );
   }
 }
+
 
 export default App;
