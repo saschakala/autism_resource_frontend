06543d1f8db7cf19f065c491648f193ac58c8a02
Sascha Kala <mxsaschakala@gmail.com>
2021-01-22 14:21:32 -0500

    experimenting with bootstrap Card and Carddeck components

--- src/components/SourceThumbnails.js
+++ src/components/SourceThumbnails.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 const SourceThumbnails = () => {
     return (
-        <Container>
-            <div>test</div>
+        <Container align="center">
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" />
+                    <Card.Body>
+                        <Card.Title>Books</Card.Title>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <Card.Img variant="top" />
+                    <Card.Body>
+                        <Card.Title>Articles</Card.Title>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <Card.Img variant="top" />
+                    <Card.Body>
+                        <Card.Title>Social Media Accounts</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
         </Container>
 
     )
 }
 
 export default SourceThumbnails
+
+// <Row>
+// <Col>1 of 3</Col>
+// <Col>2 of 3</Col>
+// <Col>3 of 3</Col>
+// </Row>
