2a3cb2c51aae09d64a72833960320e7cb5bf4cb5
Sascha Kala <mxsaschakala@gmail.com>
2021-01-22 22:32:19 -0500

    added skeleton of iterating book props to card components in BookCards.js

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -8,12 +8,18 @@ import CardColumns from 'react-bootstrap/CardColumns';
 
 const BookCards = (props) => {
     return (
-        <div>
+        // iterating through all the books resources and displaying them on individual cards
+        <Container> 
+            <CardColumns> 
             {props.books && props.books.map(book =>
-              <li>{book.attributes.title}</li>  
-                )}
-
-      </div>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{book.attributes.title}</Card.Title>
+                    </Card.Body>
+                </Card>
+            )}
+            </CardColumns>
+        </Container>
         
     )
 }
