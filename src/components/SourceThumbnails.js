8dadd28e6f1144880d8dec1abaf899eacbc42933
Sascha Kala <mxsaschakala@gmail.com>
2021-01-22 19:35:01 -0500

    source thumbnail cards are responsive

--- src/components/SourceThumbnails.js
+++ src/components/SourceThumbnails.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'
 import CardColumns from 'react-bootstrap/CardColumns';
 
 const SourceThumbnails = () => {
@@ -14,57 +9,50 @@ const SourceThumbnails = () => {
 
             <CardColumns>
                 <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Books</Card.Title>
-                        </Card.Body>
-                    </Card>
+                    <Card.Body>
+                        <Card.Title>Books</Card.Title>
+                    </Card.Body>
+                </Card>
 
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Articles</Card.Title>
-                        </Card.Body>
-                    </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Articles</Card.Title>
+                    </Card.Body>
+                </Card>
 
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Social Media Accounts</Card.Title>
-                        </Card.Body>
-                    </Card>
-                </CardColumns>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Social Media Accounts</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardColumns>
 
-                <CardColumns>
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Multimedia</Card.Title>
-                        </Card.Body>
-                    </Card>
+            <CardColumns>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Multimedia</Card.Title>
+                    </Card.Body>
+                </Card>
 
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Miscellaneous</Card.Title>
-                        </Card.Body>
-                    </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Miscellaneous</Card.Title>
+                    </Card.Body>
+                </Card>
 
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>Queer</Card.Title>
-                        </Card.Body>
-                    </Card>
-                </CardColumns>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Queer</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardColumns>
 
-               
-                    <Card>
-                        <Card.Img variant="top" />
-                        <Card.Body>
-                            <Card.Title>By Autists</Card.Title>
-                        </Card.Body>
-                    </Card>
+            
+                <Card>
+                    <Card.Body>
+                        <Card.Title>By Autists</Card.Title>
+                    </Card.Body>
+                </Card>
               
 
             </Container>
