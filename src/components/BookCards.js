c83e43c3e80759f88241ba053febf361309f1415
“saschakala” <mxsaschakala@gmail.com>
2021-01-23 07:16:01 -0500

    added book images to book resource cards

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -10,10 +10,13 @@ const BookCards = (props) => {
     return (
         // iterating through all the books resources and displaying them on individual cards
         <Container> 
+
             <CardColumns> 
             {props.books && props.books.map(book =>
-                <Card>
+                <Card key={book.id}>
                     <Card.Body>
+                        <Card.Text></Card.Text>
+                    <Card.Img variant="top" src={book.attributes.img_url} />
                         <Card.Title>{book.attributes.title}</Card.Title>
                     </Card.Body>
                 </Card>
