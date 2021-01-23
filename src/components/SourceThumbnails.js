d54d375d42f01fb1a0e72b4814306cefceaa6761
“saschakala” <mxsaschakala@gmail.com>
2021-01-23 18:55:18 -0500

    updated thumbnail hover styling

--- src/components/SourceThumbnails.js
+++ src/components/SourceThumbnails.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
 import { useHistory } from "react-router-dom";
+import '../css/thumbnails.css';
 
 const SourceThumbnails = () => {
     const history = useHistory();
