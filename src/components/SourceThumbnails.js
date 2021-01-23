face431530c682c452d83e029c280c1480aad213
Sascha Kala <mxsaschakala@gmail.com>
2021-01-23 17:35:37 -0500

    added history and testing onClick method on thumbnails

--- src/components/SourceThumbnails.js
+++ src/components/SourceThumbnails.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import history from '../history';
 
 const SourceThumbnails = () => {
     return (
         <Container align="center"  style={{marginBottom:'3rem'}}>
 
             <CardColumns >
-                <Card id="books">
+                <Card id="books" onClick={() => history.push('/ Books')}>
                     <Card.Body>
                         <Card.Title>Books</Card.Title>
                     </Card.Body>
