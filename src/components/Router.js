392f2e735b233452f3e668c4c5433361b0c53091
Sascha Kala <mxsaschakala@gmail.com>
2021-01-23 14:35:24 -0500

    added /books route

---
+++ src/components/Router.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { Switch, Route } from 'react-router-dom'
+
+// import HomePage from '../containers/HomePage';
+// import BookPage from '../containers/BookPage'
+
+
+// const Router = (props) => {
+//     return(
+//         <Switch>
+//           {/* in most specific to less specific */}
+//           <Route path="/books" render={(props.books) => <BookPage books={props.accounts}/>}/>
+//           <Route exact path="/" component = { HomePage }/>
+//       </Switch>
+//     );
+// };
+
+// export default Router;
