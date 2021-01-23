e805bb68b13c0b120bed96a94a2fa0a4f449ab99
Sascha Kala <mxsaschakala@gmail.com>
2021-01-23 09:43:56 -0500

    cards render with long descriptions

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -3,10 +3,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Accordion from 'react-bootstrap/Accordion';
+import Collapse from 'react-bootstrap/Collapse'
+
 
 
 
 const BookCards = (props) => {
+
     return (
         // iterating through all the books resources and displaying them on individual cards
         <Container> 
@@ -14,17 +20,26 @@ const BookCards = (props) => {
             <CardColumns> 
             {props.books && props.books.map(book =>
                 <Card key={book.id}>
+                
+
                     <Card.Body>
                     <Card.Img variant="top" src={book.attributes.img_url} />
                         <Card.Title>{book.attributes.title}</Card.Title>
                         {book.attributes.creators.map(creator =>
                             <Card.Subtitle className="mb-2 text-muted">{creator.first_name} {creator.last_name}</Card.Subtitle>
                           )}
+                    </Card.Body>
+
+                    <Card.Body>
                         <Card.Text>{book.attributes.description}</Card.Text>
                     </Card.Body>
+
+                    
+                    <Button variant="primary">Go somewhere</Button>
                 </Card>
             )}
             </CardColumns>
+
         </Container>
         
     )
