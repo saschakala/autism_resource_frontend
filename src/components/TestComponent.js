67c74276d46f48931294f802f87a6df12cfdda10
“saschakala” <mxsaschakala@gmail.com>
2021-01-23 12:47:04 -0500

    added description button, need to debug so that it only shows/hides one at a time

---
+++ src/components/TestComponent.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+
+class TestComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false,
+		};
+	}
+
+	render() {
+		const { open } = this.state;
+		return (
+            
+            <Container> 
+
+            <CardColumns> 
+            {this.props.books && this.props.books.map(book =>
+                <Card key={book.id}>
+                
+
+                    <Card.Body>
+                    <Card.Img variant="top" src={book.attributes.img_url} />
+                        <Card.Title>{book.attributes.title}</Card.Title>
+                        {book.attributes.creators.map(creator =>
+                            <Card.Subtitle className="mb-2 text-muted">{creator.first_name} {creator.last_name}</Card.Subtitle>
+                          )}
+                    </Card.Body>
+
+                <Collapse in={this.state.open}>
+				    <div id={book.id}>
+                    <Card.Body>
+                        <Card.Text>{book.attributes.description}</Card.Text>
+                    </Card.Body>
+
+                    </div>
+				</Collapse>
+
+                    
+                    <Button key={book.id}
+					onClick={() => this.setState({ open: !open })}
+					aria-controls={book.id}
+					aria-expanded={open}>Description</Button>
+
+                </Card>
+            )}
+            </CardColumns>
+
+        
+				
+				
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+
+        </Container>
+		);
+	}
+}
+
+export default TestComponent
