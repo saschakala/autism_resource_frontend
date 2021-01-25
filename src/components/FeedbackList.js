1becdf79fde927b23c3aa8d0a61755456591d1b0
“saschakala” <mxsaschakala@gmail.com>
2021-01-25 10:15:26 -0500

    added form to feedback page

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
 import {connect} from 'react-redux';
 import Feedback from './Feedback.js'
 
--- src/containers/FeedbackPage.js
+++ src/containers/FeedbackPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import {connect} from 'react-redux';
 import FeedbackList from '../components/FeedbackList';
 import {fetchFeedback} from '../actions/fetchFeedback';
@@ -22,6 +22,33 @@ componentDidMount() {
                     </Row>
 
                     <FeedbackList/>
+
+                    <Row>
+                        <Col>
+                        <Form style={{paddingBottom: '2rem'}}>
+                            <Form.Group controlId="feedbackTitle">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="text" placeholder="Your thoughts..." />
+                            </Form.Group>
+
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Body</Form.Label>
+                                <Form.Control as="textarea" placeholder="...go here" />
+                            </Form.Group>
+
+                            <Form.Group controlId="feedbackName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" placeholder="Name" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+
+                        </Form>
+                        </Col>
+                    </Row>
+
 
                 </Container>
         )
